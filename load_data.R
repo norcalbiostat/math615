071e81fa8dc7c0fc546a62872b883c6292a5fcac
norcalbiostat <rdonatello@csuchico.edu>
2025-10-27 23:06:03 -0700

    add t09 and full rebuild

---
+++ load_data.R
@@ -0,0 +1,7 @@
+
+
+fev <- read.delim(here::here('slides/data/Lung_081217.txt'))
+load("C:/Users/User/Box/Data/AddHealth/addhealth_clean.Rdata")
+# load("C:/Box/Data/AddHealth/addhealth_clean.Rdata")
+#load("/Users/rdonatello/Library/CloudStorage/Box-Box/Data/AddHealth/addhealth_clean.Rdata")
+
